f19a7efdfa0cfc97b89bbcdc17d6b10a9bf0866b
Bulat <kusok.apelbsina@gmail.com>
2022-04-06 14:45:03 +0300

    feature/2: added routers

--- src/routers/tokens.py
+++ src/routers/tokens.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
-from models.token import Token_Pydantic, TokenIn_Pydantic, Token
+from typing import List
 
+from models.token import Token_Pydantic, TokenIn_Pydantic, Token
 from utils.generate_random_string import get_random_string
 
 
@@ -21,3 +22,25 @@ async def create_token(token: TokenIn_Pydantic):
         **token.dict()
     )
     return await Token_Pydantic.from_tortoise_orm(token_obj)
+
+
+@router.get('/list', response_model=List[Token_Pydantic])
+async def get_list_tokens(skip: int = 0, limit: int = 10):
+    # TODO pagination
+    return await Token_Pydantic.from_queryset(Token.all())
+
+
+@router.get('/total_supply')
+async def get_total_supply():
+    """
+    TODO me
+
+    Это API должно обращаться к контракту в блокчейне и выдавать в ответе
+    информацию о текущем Total supply токена - общем числе находящихся
+    токенов в сети.
+    Форма ответа - произвольная, в JSON-формате.
+
+    Минимальный базовый пример ответа - {"result": 10000}
+    """
+    example_data = {'result_example': 1000}
+    return example_data
